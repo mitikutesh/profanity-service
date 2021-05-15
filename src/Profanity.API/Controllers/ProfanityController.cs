51cdb3db262517bd759f0cdc00afef5f7891c59f
Mitiku Geleta <mitiku.geleta@tuni.fi>
2021-05-15 19:25:16 +0300

    add profanity controller

---
+++ src/Profanity.API/Controllers/ProfanityController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Profanity.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Profanity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfanityController : ControllerBase
+    {
+        [HttpPost("check-text-for-profanity")]
+        public async Task<ActionResult<ResponseModel>> Post([FromForm(Name = "file")] IFormFile file)
+        {
+            return Ok();
+        }
+        [HttpPost("add-words-to-profanity-list")]
+        public async Task<IActionResult> PostAddToProfanityList(string word)
+        {
+            return default;
+        }
+
+        [HttpPost("remove-word-from-prfanity-list")]
+        public async Task<IActionResult> PostRemoveFromProfanityList(string word)
+        {
+            return default;
+        }
+
+       }
+}
---
+++ src/Profanity.API/Model/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Profanity.API.Model
+{
+    public class ResponseModel
+    {
+        public bool IsProfanity { get; set; }
+        public int Count { get; set; }
+        public IEnumerable<string> FoundProfanityWords { get; set; }
+        public long ElapasedTime { get; set; }
+    }
+}
